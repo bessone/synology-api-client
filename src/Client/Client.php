e36d87e764a640c87918faf3c560e0594594de52
victorbalssa <git@victorbalssa.ca>
2022-05-27 15:10:26 -0700

    [FIX] error json.

--- src/Client/Client.php
+++ src/Client/Client.php
@@ -176,7 +176,7 @@ abstract class Client
 
         $this->log($info['http_code'], 'Response code');
         if (200 === $info['http_code']) {
-            if (preg_match('#(plain|text)#', $info['content_type'])) {
+            if (preg_match('#(plain|text|json)#', $info['content_type'])) {
                 return $this->parseRequest($result);
             }
 
